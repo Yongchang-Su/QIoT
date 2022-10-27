186513de64a7017ec520c9d7b6c5bc91cdd80bff
Yongchang-Su <ysu17@illinois.edu>
2022-10-27 15:26:18 -0500

    update

--- R/data.R
+++ R/data.R
@@ -44,3 +44,17 @@
 #' }
 #' @source NHANES, the US National Health and Nutrition Examination Survey, 2005-2006.
 "cadmium"
+
+
+
+
+#' Austria unemployment change due to unemployment benefits
+#' 
+#' This data concentrates on the reform in policies of unemployment benefits in Austria and how it influence the duration of unemployment.
+#' @docType data
+#' @keywords datasets
+#' @name unemployment
+#' @usage data(unemployment)
+#' @format A data frame with 225821 rows and 175 variables:
+#' @source Ruoqi Yu & Paul R. Rosenbaum (2022) Graded Matching for Large Observational Studies, Journal of Computational and Graphical Statistics, DOI: 10.1080/10618600.2022.2058001
+#' "unemployed"
