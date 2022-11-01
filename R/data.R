55942c0a6620a7610fdc3b5c9e548236272fb269
Yongchang-Su <ysu17@illinois.edu>
2022-10-31 19:01:16 -0500

    update

--- R/data.R
+++ R/data.R
@@ -56,5 +56,5 @@
 #' @name unemployment
 #' @usage data(unemployment)
 #' @format A data frame with 225821 rows and 175 variables:
-#' @source Ruoqi Yu & Paul R. Rosenbaum (2022) Graded Matching for Large Observational Studies, Journal of Computational and Graphical Statistics, DOI: 10.1080/10618600.2022.2058001
+#' @source Ruoqi Yu & Paul R. Rosenbaum (2022) Graded Matching for Large Observational Studies, Journal of Computational and Graphical Statistics, DOI: 10.1080/10618600.2022.2058001.
 "unemployed"
