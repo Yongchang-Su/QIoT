a0aaf0f27a303d9e51cefe397e9c667469969d8b
Yongchang-Su <ysu17@illinois.edu>
2023-04-04 17:15:08 -0500

    update

--- tests/testthat/test-t4_pval_scre.R
+++ tests/testthat/test-t4_pval_scre.R
@@ -5,5 +5,5 @@ test_that("function works", {
   Z = cadmium$z
   method.list.all = list()
   method.list.all[[1]] = list(name = "Wilcoxon")
-  expect_equal(QIoT::pval_quantile_scre(Z=Z,Y=Y,block=block,k=floor(0.9*length(Y)),c=0, method.list.all=method.list.all,switch=TRUE), list(upper = 0, lower = 0, fixed = 0))
+  expect_equal(QIoT::pval_quantile_scre(Z=Z,Y=Y,block=block,k=floor(0.9*length(Y)),c=0, method.list.all=method.list.all,switch=TRUE), list(upper = 0, lower = 0, fix = 0))
 })
